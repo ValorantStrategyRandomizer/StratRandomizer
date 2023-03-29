5e8498057e231ed4e93837cbd9d86bf01bcce2fd
Ji Kim <jikim.jk@gmail.com>
2023-03-28 18:34:40 -0700

    required commit

--- stratrandomizer/src/app/page.tsx
+++ stratrandomizer/src/app/page.tsx
@@ -8,9 +8,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
-<<<<<<< HEAD
-      Hello World
-=======
       <ul>
         <li>
           <Link href="/">Home</Link>
@@ -22,7 +19,6 @@ export default function Home() {
           <Link href="/blog/hello-world">Blog Post</Link>
         </li>
       </ul>
->>>>>>> c220a1db2e89107b3518459e80194df223da6d39
     </main>
   )
 }
