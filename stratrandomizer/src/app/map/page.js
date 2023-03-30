81a7319cf0844b852028383f7cd7eb114b55c1cd
ppark528 <ppark528@gmail.com>
2023-03-29 19:34:32 -0700

    make that ish pretty

--- stratrandomizer/src/app/map/page.js
+++ stratrandomizer/src/app/map/page.js
@@ -2,7 +2,7 @@
 // next stuff
 // packages
 import { React, Fragment, useState } from 'react';
-import { Listbox, Transition } from '@headlessui/react';
+import { Listbox, Transition, Dialog } from '@headlessui/react';
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
 // Components
 import MapImage from './MapImage';
@@ -27,14 +27,13 @@ export default function Map() {
   const [selectedMap, setSelectedMap] = useState('Ascent');
   const [selectedPosition, setSelectedPosition] = useState('Attacking');
   const [showStrat, setShowStrat] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleSelectMap = (e) => {
     setSelectedMap(e);
-    setShowStrat(false);
   };
   const handleSelectPosition = (e) => {
     setSelectedPosition(e);
-    setShowStrat(false);
   };
 
   const handleClick = (e) => {
@@ -274,6 +273,65 @@ export default function Map() {
         </div>
       </div>
       <div>{showStrat ? <RandomStrat /> : ''}</div>
+
+      <Transition appear show={showStrat} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => setShowStrat(false)}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Payment successful
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      <RandomStrat />
+                      {/* showStrat is re-rendering when clicking on "close button" */}
+                    </p>
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => setShowStrat(false)}
+                    >
+                      Got it, thanks!
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </Fragment>
   );
 }
