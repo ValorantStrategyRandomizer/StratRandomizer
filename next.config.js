ff4f4c5ae2a99d837831949cdddae2c76997ad73
Austin Julius Kim <austinjuliuskim@gmail.com>
2023-05-15 19:47:15 -0700

    Dellete weird directory between main repo and this

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  images: {
+    unoptimized: true,
+  },
+};
