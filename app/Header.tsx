70a8140d7b2e095a2c27ea4b0be72e48b7e60a41
Austin Julius Kim <austinjuliuskim@gmail.com>
2023-05-15 23:19:51 -0700

    fix another link

--- app/Header.tsx
+++ app/Header.tsx
@@ -78,7 +78,7 @@ export default function Header() {
               href={
                 process.env.NODE_ENV === 'production'
                   ? `${process.env.pathPrefix}`
-                  : "/"
+                  : '/'
               }
               className="-m-1.5 p-1.5"
             >
@@ -217,7 +217,14 @@ export default function Header() {
           <div className="fixed inset-0 z-10" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
+              <a
+                href={
+                  process.env.NODE_ENV === 'production'
+                    ? `${process.env.pathPrefix}`
+                    : '/'
+                }
+                className="-m-1.5 p-1.5"
+              >
                 <span className="sr-only">Your Company</span>
                 <Image
                   className="h-8 w-auto"
