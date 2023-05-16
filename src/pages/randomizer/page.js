5b249378cb86610cbadaad2db66437e0ca561bed
Austin Julius Kim <austinjuliuskim@gmail.com>
2023-05-15 20:52:54 -0700

    Delete pages

---
+++ src/pages/randomizer/page.js
@@ -0,0 +1,461 @@
+'use client';
+// next stuff
+// packages
+import { React, Fragment, useState } from 'react';
+import { Listbox, Transition, Dialog } from '@headlessui/react';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
+// Components
+import MapImage from './MapImage';
+import RandomStrat from './strat';
+
+const CompetitiveMapPool = [
+  'Ascent',
+  'Lotus',
+  'Pearl',
+  'Split',
+  'Fracture',
+  'Icebox',
+];
+const UnratedMapPool = [
+  ...CompetitiveMapPool,
+  'Bind', 
+  'Breeze',
+];
+const MapPool = {
+  'Competitive': {
+    'maps': [
+      ...CompetitiveMapPool
+    ]
+  },
+  'Unrated': {
+    'maps': [
+      ...UnratedMapPool
+    ]
+  },
+  'Custom': {
+    'maps': [
+      ...UnratedMapPool
+    ]
+  }
+};
+const Positions = ['Attacking', 'Defending'];
+const GameModes = [
+  'Competitive',
+  'Unrated',
+  'Custom',
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function StrategyRandomizer() {
+  const [selectedMode, setSelectedMode] = useState('Competitive');
+  const [selectedMap, setSelectedMap] = useState('Ascent');
+  const [selectedPosition, setSelectedPosition] = useState('Attacking');
+  const [showStrat, setShowStrat] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSelectGameMode = (e) => {
+    setSelectedMode(e);
+  };
+  const handleSelectMap = (e) => {
+    setSelectedMap(e);
+  };
+  const handleSelectPosition = (e) => {
+    setSelectedPosition(e);
+  };
+  const handleClick = (e) => {
+    e.preventDefault();
+    setShowStrat(true);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
+  return (
+    <Fragment>
+      <div
+        style={{
+          display: 'flex',
+          width: '100vw',
+          height: '100vh',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{
+            width: '30vw',
+            padding: '20px',
+          }}
+        >
+          <form onSubmit={handleSubmit}>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              <div>
+                <Listbox value={selectedMode} onChange={setSelectedMode}>
+                  {({ open }) => (
+                    <>
+                      <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">
+                        Select Game Mode
+                      </Listbox.Label>
+                      <div className="relative mt-2">
+                        <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                          <span className="flex items-center">
+                            <span className="ml-3 block truncate">
+                              {selectedMode}
+                            </span>
+                          </span>
+                          <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                            <ChevronUpDownIcon
+                              className="h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                          </span>
+                        </Listbox.Button>
+
+                        <Transition
+                          show={open}
+                          as={Fragment}
+                          leave="transition ease-in duration-100"
+                          leaveFrom="opacity-100"
+                          leaveTo="opacity-0"
+                        >
+                          <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                            {GameModes.map((modeName, index) => (
+                              <Listbox.Option
+                                key={`${modeName}-${index}`}
+                                className={({ active }) =>
+                                  classNames(
+                                    active
+                                      ? 'bg-indigo-600 text-white'
+                                      : 'text-gray-900',
+                                    'relative cursor-default select-none py-2 pl-3 pr-9'
+                                  )
+                                }
+                                value={modeName}
+                              >
+                                {({ selected, active }) => (
+                                  <>
+                                    <div className="flex items-center">
+                                      <span
+                                        className={classNames(
+                                          selected
+                                            ? 'font-semibold'
+                                            : 'font-normal',
+                                          'ml-3 block truncate'
+                                        )}
+                                      >
+                                        {modeName}
+                                      </span>
+                                    </div>
+
+                                    {selected ? (
+                                      <span
+                                        className={classNames(
+                                          active
+                                            ? 'text-white'
+                                            : 'text-indigo-600',
+                                          'absolute inset-y-0 right-0 flex items-center pr-4'
+                                        )}
+                                      >
+                                        <CheckIcon
+                                          className="h-5 w-5"
+                                          aria-hidden="true"
+                                        />
+                                      </span>
+                                    ) : null}
+                                  </>
+                                )}
+                              </Listbox.Option>
+                            ))}
+                          </Listbox.Options>
+                        </Transition>
+                      </div>
+                    </>
+                  )}
+                </Listbox>
+              </div>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  marginTop: '20px',
+                }}
+              >
+                <Listbox value={selectedMap} onChange={handleSelectMap}>
+                  {({ open }) => (
+                    <>
+                      <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">
+                        Select Map
+                      </Listbox.Label>
+                      <div className="relative mt-2">
+                        <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                          <span className="flex items-center">
+                            <span className="ml-3 block truncate">
+                              {selectedMap}
+                            </span>
+                          </span>
+                          <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                            <ChevronUpDownIcon
+                              className="h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                          </span>
+                        </Listbox.Button>
+
+                        <Transition
+                          show={open}
+                          as={Fragment}
+                          leave="transition ease-in duration-100"
+                          leaveFrom="opacity-100"
+                          leaveTo="opacity-0"
+                        >
+                          <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                            {MapPool[selectedMode]?.maps?.map((mapName, index) => (
+                              <Listbox.Option
+                                key={`${mapName}-${index}`}
+                                className={({ active }) =>
+                                  classNames(
+                                    active
+                                      ? 'bg-indigo-600 text-white'
+                                      : 'text-gray-900',
+                                    'relative cursor-default select-none py-2 pl-3 pr-9'
+                                  )
+                                }
+                                value={mapName}
+                              >
+                                {({ selected, active }) => (
+                                  <>
+                                    <div className="flex items-center">
+                                      <span
+                                        className={classNames(
+                                          selected
+                                            ? 'font-semibold'
+                                            : 'font-normal',
+                                          'ml-3 block truncate'
+                                        )}
+                                      >
+                                        {mapName}
+                                      </span>
+                                    </div>
+
+                                    {selected ? (
+                                      <span
+                                        className={classNames(
+                                          active
+                                            ? 'text-white'
+                                            : 'text-indigo-600',
+                                          'absolute inset-y-0 right-0 flex items-center pr-4'
+                                        )}
+                                      >
+                                        <CheckIcon
+                                          className="h-5 w-5"
+                                          aria-hidden="true"
+                                        />
+                                      </span>
+                                    ) : null}
+                                  </>
+                                )}
+                              </Listbox.Option>
+                            ))}
+                          </Listbox.Options>
+                        </Transition>
+                      </div>
+                    </>
+                  )}
+                </Listbox>
+              </div>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  marginTop: '20px',
+                }}
+              >
+                <Listbox
+                  value={selectedPosition}
+                  onChange={handleSelectPosition}
+                >
+                  {({ open }) => (
+                    <>
+                      <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">
+                        Select Position
+                      </Listbox.Label>
+                      <div className="relative mt-2">
+                        <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                          <span className="flex items-center">
+                            <span className="ml-3 block truncate">
+                              {selectedPosition}
+                            </span>
+                          </span>
+                          <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                            <ChevronUpDownIcon
+                              className="h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                          </span>
+                        </Listbox.Button>
+
+                        <Transition
+                          show={open}
+                          as={Fragment}
+                          leave="transition ease-in duration-100"
+                          leaveFrom="opacity-100"
+                          leaveTo="opacity-0"
+                        >
+                          <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                            {Positions.map((pos, index) => (
+                              <Listbox.Option
+                                key={`${pos}-${index}`}
+                                className={({ active }) =>
+                                  classNames(
+                                    active
+                                      ? 'bg-indigo-600 text-white'
+                                      : 'text-gray-900',
+                                    'relative cursor-default select-none py-2 pl-3 pr-9'
+                                  )
+                                }
+                                value={pos}
+                              >
+                                {({ selected, active }) => (
+                                  <>
+                                    <div className="flex items-center">
+                                      <span
+                                        className={classNames(
+                                          selected
+                                            ? 'font-semibold'
+                                            : 'font-normal',
+                                          'ml-3 block truncate'
+                                        )}
+                                      >
+                                        {pos}
+                                      </span>
+                                    </div>
+
+                                    {selected ? (
+                                      <span
+                                        className={classNames(
+                                          active
+                                            ? 'text-white'
+                                            : 'text-indigo-600',
+                                          'absolute inset-y-0 right-0 flex items-center pr-4'
+                                        )}
+                                      >
+                                        <CheckIcon
+                                          className="h-5 w-5"
+                                          aria-hidden="true"
+                                        />
+                                      </span>
+                                    ) : null}
+                                  </>
+                                )}
+                              </Listbox.Option>
+                            ))}
+                          </Listbox.Options>
+                        </Transition>
+                      </div>
+                    </>
+                  )}
+                </Listbox>
+              </div>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  marginTop: '20px',
+                }}
+              >
+                <button
+                  type="submit"
+                  onClick={handleClick}
+                  className="rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Go
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div>
+          <MapImage
+            selectedMap={selectedMap}
+            selectedPosition={selectedPosition}
+          />
+        </div>
+      </div>
+      {/* <div>{showStrat ? <RandomStrat /> : ''}</div> */}
+
+      <Transition appear show={showStrat} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => setShowStrat(false)}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <div className="flex justify-end">
+                    <button type="button" onClick={() => setShowStrat(false)}>
+                      <strong>&times;</strong>
+                    </button>
+                  </div>
+
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Here is your strat you iron
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      <RandomStrat />
+                      {/* showStrat is re-rendering when clicking on "close button" */}
+                    </p>
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => setShowStrat(false)}
+                    >
+                      Got it, thanks!
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </Fragment>
+  );
+}
